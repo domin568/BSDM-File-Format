5bb99f7ffda81288251a72e77588af20bcf9e02c
Domin568 <domin568@gmail.com>
2020-01-22 18:27:57 +0100

    batalia 22.01

--- bmp.cpp
+++ bmp.cpp
@@ -188,60 +188,188 @@ uint8_t *ConvertStringtoBDSMrawData(std::string decompressed,int BSDMDataSize)
 }
 
 
-void ditheringColor(uint8_t* src, BITMAPINFOHEADER & infoHeader) 
+RGB_color defaultcolors[32] = 
 {
-    uint32_t dataSizeWithoutPitches = infoHeader.biWidth * infoHeader.biHeight * 3;
-    uint32_t width = infoHeader.biWidth;
-    RGB_color newColor;
-    for (int i = 0; i < dataSizeWithoutPitches; i += 3)
+    {0,0,0},
+    {0,0,255},
+    {0,85,0},
+    {0,85,255},
+    {0,170,0},
+    {0,170,255},
+    {0,255,0},
+    {0,255,255},
+    {85,0,0},
+    {85,0,255},
+    {85,85,0},
+    {85,85,255},
+    {85,170,0},
+    {85,170,255},
+    {85,255,0},
+    {85,255,255},
+    {170,0,0},
+    {170,0,255},
+    {170,85,0},
+    {170,85,255},
+    {170,170,0},
+    {170,170,255},
+    {170,255,0},
+    {170,255,255},
+    {255,0,0},
+    {255,0,255},
+    {255,85,0},
+    {255,85,255},
+    {255,170,0},
+    {255,170,255},
+    {255,255,0},
+    {255,255,255}
+};
+
+BSDM_PALETTE defaultPalette
+{
+    32, defaultcolors
+};
+
+RGB_color graycolors[32] = 
+{
+    {0,0,0},
+    {8,8,8},
+    {16,16,16},
+    {24,24,24},
+    {32,32,32},
+    {41,41,41},
+    {49,49,49},
+    {57,57,57},
+    {65,65,65},
+    {74,74,74},
+    {82,82,82},
+    {90,90,90},
+    {98,98,98},
+    {106,106,106},
+    {115,115,115},
+    {123,123,123},
+    {131,131,131},
+    {139,139,139},
+    {148,148,148},
+    {156,156,156},
+    {164,164,164},
+    {172,172,172},
+    {180,180,180},
+    {189,189,189},
+    {197,197,197},
+    {205,205,205},
+    {213,213,213},
+    {222,222,222},
+    {230,230,230},
+    {238,238,238},
+    {246,246,246},
+    {255,255,255}
+ };
+ 
+ BSDM_PALETTE grayPalette
+ {
+     32,graycolors
+ };
+
+ uint8_t findClosestColorIndexFromPalette(RGB_color needle, BSDM_PALETTE * palette)
+{
+    // search for smallest Euclidean distance
+    uint8_t index;
+    uint32_t d, minimal = 255 * 255 * 3 + 1;
+    uint32_t dR, dG, dB;
+    RGB_color current;
+
+    for (int i = 0; i < palette->numberOfColors; i++) 
     {
-        uint8_t oldR = src[i];
-        uint8_t oldG = src[i + 1];
-        uint8_t oldB = src[i + 2];
-        RGB_color oldColor = { oldR,oldG,oldB };
-        //  newColor = find_closest_palette_color(oldColor);
-        uint8_t newR = newColor.r;
-        uint8_t newG = newColor.g;
-        uint8_t newB = newColor.b;
+        current = palette->colors[i];
+        dR = needle.r - current.r;
+        dG = needle.g - current.g;
+        dB = needle.b - current.b;
+        d = dR * dR + dG * dG + dB * dB;
+        if (d < minimal) 
+        {
+            minimal = d;
+            index = i;
+        }
+    }
+    return index;
+}
 
-        src[i] = newR;
-        src[i + 1] = newG;
-        src[i + 2] = newB;
-
-        int errorR = src[i] - newR;             //not sure, maybe change places with 181-183
-        int errorG = src[i + 1] - newG;
-        int errorB = src[i + 2] - newB;
-
-        src[i + 3] = src[i + 3] + errorR * (7.0f / 16.0f);
-        src[i + 4] = src[i + 4] + errorG * (7.0f / 16.0f);
-        src[i + 5] = src[i + 5] + errorB * (7.0f / 16.0f);
-
-        src[i + width - 3] = src[i + width - 3] + errorR * (3.0f / 16.0f);
-        src[i + width - 2] = src[i + width - 2] + errorG * (3.0f / 16.0f);
-        src[i + width - 1] = src[i + width - 1] + errorB * (3.0f / 16.0f);
-
-        src[i + width] = src[i + width] + errorR * (5.0f / 16.0f);
-        src[i + width + 1] = src[i + width + 1] + errorG * (5.0f / 16.0f);
-        src[i + width + 2] = src[i + width + 2] + errorB * (5.0f / 16.0f);
-
-        src[i + width + 3] = src[i + width + 3] + errorR * (1.0f / 16.0f);
-        src[i + width + 4] = src[i + width + 4] + errorG * (1.0f / 16.0f);
-        src[i + width + 5] = src[i + width + 5] + errorB * (1.0f / 16.0f);
-
-
-        /*pixels[y][x + 1].red   = pixels[y][x + 1].red   + ErrorR * (7.0f / 16.0f);
-            pixels[y][x + 1].green = pixels[y][x + 1].green + ErrorG * (7.0f / 16.0f);
-            pixels[y][x + 1].blue  = pixels[y][x + 1].blue  + ErrorB * (7.0f / 16.0f);
-            pixels[y + 1][x - 1].red    = pixels[y + 1][x - 1].red   + ErrorR * (3.0f / 16.0f);
-            pixels[y + 1][x - 1].green  = pixels[y + 1][x - 1].green + ErrorG * (3.0f / 16.0f);
-            pixels[y + 1][x - 1].blue   = pixels[y + 1][x - 1].blue  + ErrorB * (3.0f / 16.0f);
-            pixels[y + 1][x].red    = pixels[y + 1][x].red   + ErrorR * (5.0f / 16.0f);
-            pixels[y + 1][x].green  = pixels[y + 1][x].green + ErrorG * (5.0f / 16.0f);
-            pixels[y + 1][x].blue   = pixels[y + 1][x].blue  + ErrorB * (5.0f / 16.0f);
-            pixels[y + 1][x + 1].red    = pixels[y + 1][x + 1].red   + ErrorR * (1.0f / 16.0f);
-            pixels[y + 1][x + 1].green  = pixels[y + 1][x + 1].green + ErrorG * (1.0f / 16.0f);
-            pixels[y + 1][x + 1].blue   = pixels[y + 1][x + 1].blue  + ErrorB * (1.0f / 16.0f);*/
-
+void ditheringColor(uint8_t * src, uint8_t * dst, const BITMAPINFOHEADER & infoHeader, BSDM_PALETTE * colorTable)
+{
+  RGB_color currentPixel;
+  RGB_color choosenColor;
+  RGB_color * srcColor = (RGB_color *) src;
+  RGB_color * dstColor = (RGB_color *) dst;
+ 
+  // Storage for errors in color conversion
+  float **errorsR = new float *[infoHeader.biWidth + 2];
+  float **errorsG = new float *[infoHeader.biWidth + 2];
+  float **errorsB = new float *[infoHeader.biWidth + 2];
+  for (int i = 0; i < infoHeader.biWidth + 2; ++i) {
+    errorsR[i] = new float[infoHeader.biHeight + 2]{0};
+    errorsG[i] = new float[infoHeader.biHeight + 2]{0};
+    errorsB[i] = new float[infoHeader.biHeight + 2]{0};
+  }
+ 
+  int shift = 1; // To not exceed table range
+  float currentErrorR = 0, currentErrorG = 0, currentErrorB = 0;
+ 
+  for (int y = 0; y < infoHeader.biHeight; y++)
+    for (int x = 0; x < infoHeader.biWidth; x++) {
+      //currentPixel = bmp.getPixel(x, y);
+       currentPixel = srcColor[y * infoHeader.biWidth + x];
+ 
+      // Passing current error to current Pixel with overflow protetcion
+      if (currentPixel.r + errorsR[x + shift][y] > 255)
+        currentPixel.r = 255;
+      else if (currentPixel.r + errorsR[x + shift][y] < 0)
+        currentPixel.r = 0;
+      else
+        currentPixel.r += errorsR[x + shift][y];
+ 
+      if (currentPixel.g + errorsG[x + shift][y] > 255)
+        currentPixel.g = 255;
+      else if (currentPixel.g + errorsG[x + shift][y] < 0)
+        currentPixel.g = 0;
+      else
+        currentPixel.g += errorsG[x + shift][y];
+ 
+      if (currentPixel.b + errorsB[x + shift][y] > 255)
+        currentPixel.b = 255;
+      else if (currentPixel.b + errorsB[x + shift][y] < 0)
+        currentPixel.b = 0;
+      else
+        currentPixel.b += errorsB[x + shift][y];
+ 
+      // Matching color from give table
+      //choosenColor =
+      //    colorTable[findClosestColorIndexFromTable(currentPixel, colorTable)];
+      choosenColor = colorTable->colors[findClosestColorIndexFromPalette(currentPixel, colorTable)];
+ 
+      // Calculating error from current pixel
+      currentErrorR = currentPixel.r - choosenColor.r;
+      currentErrorG = currentPixel.g - choosenColor.g;
+      currentErrorB = currentPixel.b - choosenColor.b;
+    
+      dstColor[y * infoHeader.biWidth + x] = choosenColor;
+      //bmp.setPixel(x, y, choosenColor);
+ 
+      // Pasing errors from current pixels to rest as determined in
+      // Floyd–Steinber alg
+      errorsR[x + 1 + shift][y] += (currentErrorR * 7.0 / 16.0);
+      errorsR[x + 1 + shift][y + 1] += (currentErrorR * 1.0 / 16.0);
+      errorsR[x + shift][y + 1] += (currentErrorR * 5.0 / 16.0);
+      errorsR[x - 1 + shift][y + 1] += (currentErrorR * 3.0 / 16.0);
+ 
+      errorsG[x + 1 + shift][y] += (currentErrorG * 7.0 / 16.0);
+      errorsG[x + 1 + shift][y + 1] += (currentErrorG * 1.0 / 16.0);
+      errorsG[x + shift][y + 1] += (currentErrorG * 5.0 / 16.0);
+      errorsG[x - 1 + shift][y + 1] += (currentErrorG * 3.0 / 16.0);
+ 
+      errorsB[x + 1 + shift][y] += (currentErrorB * 7.0 / 16.0);
+      errorsB[x + 1 + shift][y + 1] += (currentErrorB * 1.0 / 16.0);
+      errorsB[x + shift][y + 1] += (currentErrorB * 5.0 / 16.0);
+      errorsB[x - 1 + shift][y + 1] += (currentErrorB * 3.0 / 16.0);
     }
 }
 
@@ -340,38 +468,51 @@ uint8_t * readRawBSDMData (FILE * bsdmFile, const BSDMHEADER & header)
     fread(data, 1, rawDataSize, bsdmFile);
     return data;
 }
-void transcodePixels5bits (const uint8_t * src, uint8_t * dst, bool direction, const BITMAPINFOHEADER & infoHeader)
+void grayscaleIn5bits(const uint8_t* src, uint8_t* dst, const BITMAPINFOHEADER& infoHeader) 
+{
+    uint32_t dataSizeWithoutPitches = infoHeader.biWidth * infoHeader.biHeight * 3;
+    for (int i = 0; i < dataSizeWithoutPitches; i+=3) 
+    {
+        uint8_t BW;
+        uint8_t r = src[i];
+        uint8_t g = src[i + 1];
+        uint8_t b = src[i + 2];
+        BW = 0.299 * r + 0.587 * g + 0.114 * b;
+        dst[i/3] = (BW>>3);
+    }
+}
+void transcodePixels5bits (const uint8_t * src, uint8_t * dst, const BITMAPINFOHEADER & infoHeader)
 {
     // direction == 0 --> BMP to BSDM
     // direction == 1 --> BSDM to BMP
     
-    if (direction == 0)
+    uint32_t dataSizeWithoutPitches = infoHeader.biWidth * infoHeader.biHeight *3;
+    // assuming BMP is 24 bpp
+    // blue is least important color so using one bit less for this color
+    for (int i = 0; i < dataSizeWithoutPitches; i+=3)
     {
-        uint32_t dataSizeWithoutPitches = infoHeader.biWidth * infoHeader.biHeight *3;
-        // assuming BMP is 24 bpp
-        // blue is least important color so using one bit less for this color
-        for (int i = 0; i < dataSizeWithoutPitches; i+=3)
-        {
-            uint8_t r = src[i];
-            uint8_t g = src[i+1];
-            uint8_t b = src[i+2];
+        uint8_t r = src[i];
+        uint8_t g = src[i+1];
+        uint8_t b = src[i+2];
 
-            // 1
-            {
-                r = r >> 6;
-                g = g >> 6;
-                b = b >> 7;
-                dst[i/3] = r << 3;
-                dst[i/3] = dst[i/3] | (g << 1);
-                dst[i/3] = dst[i/3] | b;
-            }
-        }
-        
-    }
-    else if (direction == 1)
+        r = r >> 6;
+        g = g >> 6;
+        b = b >> 7;
+        dst[i/3] = r << 3;
+        dst[i/3] = dst[i/3] | (g << 1);
+        dst[i/3] = dst[i/3] | b;
+    }   
+}
+void transcodePixels5bitsGrayscale (const uint8_t * src, uint8_t * dst, const BITMAPINFOHEADER & infoHeader)
+{    
+    uint32_t dataSizeWithoutPitches = infoHeader.biWidth * infoHeader.biHeight *3;
+
+    for (int i = 0; i < dataSizeWithoutPitches; i+=3)
     {
+        uint8_t r = src[i];
 
-    }
+        dst[i/3] = r >> 3;
+    }   
 }
 
 typedef struct 
@@ -621,30 +762,6 @@ void convertSaveBMPPalette (FILE * fBSDM, FILE * fBMP, BSDM_PALETTE & palette, c
     }
 }
 
-uint8_t findClosestColorIndexFromPalette(RGB_color needle, BSDM_PALETTE * palette)
-{
-    // search for smallest Euclidean distance
-    uint8_t index;
-    uint32_t d, minimal = 255 * 255 * 3 + 1;
-    uint32_t dR, dG, dB;
-    RGB_color current;
-
-    for (int i = 0; i < palette->numberOfColors; i++) 
-    {
-        current = palette->colors[i];
-        dR = needle.r - current.r;
-        dG = needle.g - current.g;
-        dB = needle.b - current.b;
-        d = dR * dR + dG * dG + dB * dB;
-        if (d < minimal) 
-        {
-            minimal = d;
-            index = i;
-        }
-    }
-    return index;
-}
-
 void saveColorNumbers (FILE * fBSDM, BSDM_PALETTE * palette, RGB_color * rawBMPBitmapData, const BITMAPINFOHEADER & infoHeader)
 {
     RGB_color * pixel;
@@ -658,7 +775,18 @@ void saveColorNumbers (FILE * fBSDM, BSDM_PALETTE * palette, RGB_color * rawBMPB
         }
     }
 }
+uint8_t * fiveBitToRGB (uint8_t * src, const BITMAPINFOHEADER & infoHeader)
+{
+    RGB_color * data = new RGB_color [infoHeader.biWidth * infoHeader.biHeight];
+    for (int i = 0 ; i < infoHeader.biHeight * infoHeader.biWidth; i++)
+    {
+        data[i].r = src[i] * (255/31);
+        data[i].g = src[i] * (255/31);
+        data[i].b = src[i] * (255/31);
+    }
 
+    return (uint8_t *) data;
+}
 void convertSaveBSDM (FILE * fBSDM, const BITMAPFILEHEADER & fileHeader, const BITMAPINFOHEADER & infoHeader, uint8_t * rawBMPBitmapData, bool mode, bool isDedicatedPaletteUsed, bool dithering)
 {
     BSDMHEADER bsdm_header;
@@ -679,7 +807,8 @@ void convertSaveBSDM (FILE * fBSDM, const BITMAPFILEHEADER & fileHeader, const B
 
     fwrite (&bsdm_header,1,sizeof(BSDMHEADER),fBSDM);
 
-    uint8_t * rawBSDMBitmapData = new uint8_t [bsdm_header.width * bsdm_header.height];
+    uint8_t * rawBSDMBitmapData = new uint8_t [bsdm_header.width * bsdm_header.height]; 
+    uint8_t * rawDitheringData = new uint8_t [bsdm_header.width * bsdm_header.height * 3]; 
 
     if (bsdm_header.isCustomPalette)
     {  
@@ -688,21 +817,57 @@ void convertSaveBSDM (FILE * fBSDM, const BITMAPFILEHEADER & fileHeader, const B
         memcpy (rawBMPBitmapDataQuantinize,rawBMPBitmapData,s);
 
         BSDM_PALETTE * palette = MCQuantizeData((MCTriplet *)rawBMPBitmapDataQuantinize,bsdm_header.width * bsdm_header.height,5);
-        saveBSDMPalette(fBSDM,palette);
-        saveColorNumbers (fBSDM,palette,(RGB_color *)rawBMPBitmapData,infoHeader);
+        if (dithering)
+        { 
+            // WIN
+            std::cout << "Paleta custom with dithering" << std::endl;
+            ditheringColor(rawBMPBitmapData,rawBSDMBitmapData,infoHeader,palette);
+            saveBSDMPalette(fBSDM,palette);
+            saveColorNumbers (fBSDM,palette,(RGB_color *)rawBSDMBitmapData,infoHeader);
+        }
+        else 
+        {
+            // wIN
+            std::cout << "Paleta custom only\n";
+            saveBSDMPalette(fBSDM,palette);
+            saveColorNumbers (fBSDM,palette,(RGB_color *)rawBMPBitmapData,infoHeader);
+        }
         free (rawBMPBitmapDataQuantinize);
     }
     else 
     {  
-        transcodePixels5bits (rawBMPBitmapData,rawBSDMBitmapData,0, infoHeader); 
-        //lzw_compression () // <------------- TUTAJ MUSI BYĆ KOMPRESJA LZW na rawBSDMBitmapData
-        fwrite(rawBSDMBitmapData,1,infoHeader.biSizeImage/3,fBSDM);
+        if (dithering)
+        {
+            if (mode)
+            {
+                std::cout << "Dithering with grayscale on default palette" << std::endl;
+                grayscaleIn5bits(rawBMPBitmapData,rawBSDMBitmapData,infoHeader);
+                uint8_t * data = fiveBitToRGB (rawBSDMBitmapData,infoHeader);
+                ditheringColor(data,rawDitheringData,infoHeader,&grayPalette);
+                transcodePixels5bitsGrayscale(rawDitheringData,rawBSDMBitmapData,infoHeader);
+            }
+            else
+            {
+                //WIN
+                std::cout << "Dithering only on default palette" << std::endl;
+                ditheringColor(rawBMPBitmapData,rawDitheringData,infoHeader,&defaultPalette);
+                transcodePixels5bits(rawDitheringData,rawBSDMBitmapData,infoHeader);
+            }
+        }
+        else
+        {
+            // WIN
+            std::cout << "Grayscale only on default palette" << std::endl;
+            grayscaleIn5bits(rawBMPBitmapData,rawBSDMBitmapData,infoHeader);
+            
+        }
+        fwrite(rawBSDMBitmapData,1,infoHeader.biWidth * infoHeader.biHeight,fBSDM);
     }
 
     fclose(fBSDM);
 
 }
-void convertSaveBMP (FILE * fBMP, uint8_t * rawBSDMBitmapData, const BSDMHEADER & bsdmHeader)
+void convertSaveBMP (FILE * fBMP, uint8_t * rawBSDMBitmapData, const BSDMHEADER & bsdmHeader, bool grayscale, bool dithering)
 {
     BITMAPFILEHEADER fileHeader;
     BITMAPINFOHEADER infoHeader;
@@ -734,19 +899,39 @@ void convertSaveBMP (FILE * fBMP, uint8_t * rawBSDMBitmapData, const BSDMHEADER 
     uint32_t pitch = (infoHeader.biWidth*3 + 3) & ~3U;
     uint32_t widthPitchDiff = (pitch - bsdmHeader.width) * 3;
 
-    for (int i = 0; i < std::abs(infoHeader.biHeight); i++) // negative height
+    if (grayscale)
     {
-        for (int j = 0; j < pitch; j+=3)
+        for (int i = 0; i < std::abs(infoHeader.biHeight); i++) // negative height
         {
-            rawBitmapData[ i * pitch + j    ] = j < infoHeader.biWidth * 3 ? \
-            (rawBSDMBitmapData[ i * bsdmHeader.width + j / 3] & 0x1)      << 7  : 0xCD;  // B
+            for (int j = 0; j < pitch; j+=3)
+            {
+                rawBitmapData[ i * pitch + j    ] = j < infoHeader.biWidth * 3 ? \
+                (rawBSDMBitmapData[ i * bsdmHeader.width + j / 3] << 3 ) : 0xCD;  // BW
 
-            rawBitmapData[ i * pitch + j + 1] = j < infoHeader.biWidth * 3 ? \
-            (rawBSDMBitmapData[ i * bsdmHeader.width + j / 3] & 0x6)  << 5  : 0xCD;  // G
-
-            rawBitmapData[ i * pitch + j + 2] = j < infoHeader.biWidth * 3 ? \
-            (rawBSDMBitmapData[ i * bsdmHeader.width + j / 3] & 0x18) << 3  : 0xCD; // R
-        } 
+                rawBitmapData[ i * pitch + j + 1] = j < infoHeader.biWidth * 3 ? \
+                (rawBSDMBitmapData[ i * bsdmHeader.width + j / 3] << 3 )  : 0xCD;  // BW
+    
+                rawBitmapData[ i * pitch + j + 2] = j < infoHeader.biWidth * 3 ? \
+                (rawBSDMBitmapData[ i * bsdmHeader.width + j / 3] << 3)  : 0xCD; // BW
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < std::abs(infoHeader.biHeight); i++) // negative height
+        {
+            for (int j = 0; j < pitch; j+=3)
+            {
+                rawBitmapData[ i * pitch + j    ] = j < infoHeader.biWidth * 3 ? \
+                (rawBSDMBitmapData[ i * bsdmHeader.width + j / 3] & 0x1)      << 7  : 0xCD;  // B
+    
+               rawBitmapData[ i * pitch + j + 1] = j < infoHeader.biWidth * 3 ? \
+                (rawBSDMBitmapData[ i * bsdmHeader.width + j / 3] & 0x6)  << 5  : 0xCD;  // G
+    
+               rawBitmapData[ i * pitch + j + 2] = j < infoHeader.biWidth * 3 ? \
+                (rawBSDMBitmapData[ i * bsdmHeader.width + j / 3] & 0x18) << 3  : 0xCD; // R
+            } 
+        }
     }
     fwrite (rawBitmapData,infoHeader.biSizeImage,1,fBMP);
 }
@@ -833,7 +1018,7 @@ int main(int argc, const char * argv[])
         {
             uint8_t * rawBSDMBitmapData = readRawBSDMData (in,BSDMheaderIN);
             //lzw_decompress // <--------- dekompresja surowych danych bitmapy BSDM 
-            convertSaveBMP (out,rawBSDMBitmapData,BSDMheaderIN);
+            convertSaveBMP (out,rawBSDMBitmapData,BSDMheaderIN,grayscale,dithering);
         }
     }
     else 
